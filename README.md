cd6bba7a28933b39b86d46edf6563e1a86226c0b
mouseandthebillionaire <matt@mouseandthebillionaire.com>
2024-06-10 15:37:20 -0400

    makings: _template and initial experiment
    First experiment is just testing that I can hook RNBO up to p5 using some tutorial that I found but don't remember what it is anymore. I will find later for posterity.
    
    _template file here is a bare bones version of the RNBO p5 hookup that I can use as a starting point for all future sketches

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Reflective Audio Game Experiences
 
 Sandbox/Playground for PhD research
+
+A series of small experiments for reflection in games, primarily focusing on experimental interactions in p5.js and sound design in RNBO.
---
+++ _template/script.js
@@ -0,0 +1,71 @@
+let canvas;
+let w, h;
+let device;
+let sketchStarted=false;
+let audioContext;
+
+let parameter;
+
+function setup(){
+    w = window.innerWidth;
+    h = window.innerHeight;
+    canvas = createCanvas(w,h)
+    background('white')
+    fill('red')
+    noStroke()
+
+    startButton = createButton('Start')
+    startButton.position(w/2, h/2)
+    startButton.mousePressed(resumeAudio)
+    audioContext = getAudioContext()
+
+    rnboSetup(audioContext);
+}
+async function rnboSetup(context){
+    const outputNode = context.createGain();
+    outputNode.connect(context.destination);
+
+    const { createDevice } = RNBO;
+    await context.resume();
+    const rawPatcher = await fetch('exports/patch.export.json');
+    const patcher = await rawPatcher.json();
+
+    device = await createDevice ({context: context, patcher});
+
+    // signal chain
+    synth.connect(device.node);
+    device.node.connect(outputNode);
+
+    //parameter = device.parametersById.get('parameterName');
+}
+
+function mousePressed(){
+    if(sketchStarted == true){
+
+        fill('red');
+        ellipse(mouseX, mouseY, random(200));
+
+    }
+}
+
+function applyParameters() {
+    /* Apply the parameters
+    let parameterAMT = map(mouseX, 0, h, 0.0, 1.0);
+    if(parameter) parameter.normalizedValue = parameterAMT;
+    */
+}
+
+function resumeAudio(){
+    sketchStarted = true;
+    startButton.style('opacity', '0')
+
+    if(getAudioContext().state !== 'running'){
+        audioContext.resume()
+    }
+}
+
+function draw(){
+    background('rgba(255, 255, 255, 0.15)');
+
+
+}
