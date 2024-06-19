d756ae3656dd30a3a26cbf024dd3f11ac9ae68c3
mouseandthebillionaire <matt@mouseandthebillionaire.com>
2024-06-19 14:57:56 -0400

    makings: experiment 1 exploring simple interactions and a correlation of success with the reduction of effort

--- 01/script.js
+++ 01/script.js
@@ -1,24 +1,29 @@
 let canvas;
-let w, h;
+let w, h, ellipseX;
+let xoff = 0;
+let r = 25;
 let device;
 let sketchStarted=false;
 let audioContext;
 
 // RNBO Control
-let parameter;
+let paramDrive, paramPlaySpeed, paramLoop, paramDelay;
 
 function setup(){
     w = window.innerWidth;
     h = window.innerHeight;
     canvas = createCanvas(w,h)
     background('white')
-    fill('black')
-    noStroke()
+    fill('black');
+    noStroke();
 
     startButton = createButton('Start')
     startButton.position(w/2, h/2)
-    startButton.mousePressed(resumeAudio)
-    audioContext = getAudioContext()
+    startButton.mousePressed(resumeAudio);
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    // set up synth? Can we do without this at some point?
+    // synth = new p5.MonoSynth();
 
     rnboSetup(audioContext);
 }
@@ -28,45 +33,79 @@ async function rnboSetup(context){
 
     const { createDevice } = RNBO;
     await context.resume();
-    const rawPatcher = await fetch('exports/patch.export.json');
+    const rawPatcher = await fetch('audio/patch.export.json');
     const patcher = await rawPatcher.json();
 
-    device = await createDevice ({context: context, patcher});
-
-    // signal chain
-    synth.connect(device.node);
+    device = await createDevice ({context, patcher});
     device.node.connect(outputNode);
 
-    //parameter = device.parametersById.get('parameterName');
+    // (Optional) Fetch the dependencies
+    let dependencies = [];
+    try {
+        const dependenciesResponse = await fetch("audio/dependencies.json");
+        dependencies = await dependenciesResponse.json();
+
+        // Prepend "export" to any file dependencies
+        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "audio/" + d.file }) : d);
+    } catch (e) {}
+
+    // (Optional) Load the samples
+    if (dependencies.length)
+        await device.loadDataBufferDependencies(dependencies);
+
+    // Parameters
+    paramDrive = device.parametersById.get('drive');
+    paramPlaySpeed = device.parametersById.get('playSpeed');
+    paramLoop = device.parametersById.get('chimeLoop');
+    paramDelay = device.parametersById.get('delayFB');
 }
 
 function mousePressed(){
-    if(sketchStarted == true){
 
-        fill('red');
-        ellipse(mouseX, mouseY, random(200));
-
-    }
 }
 
 function applyParameters() {
-    /* Apply the parameters
-    let parameterAMT = map(mouseX, 0, h, 0.0, 1.0);
-    if(parameter) parameter.normalizedValue = parameterAMT;
-    */
+    // Apply the parameters
+    let playSpeedAMT = map(r, 25, w/2, 1.0, 0.0);
+    let driveAMT = map(r, 25, w/2, 0.0, 1.0);
+    let loopAMT =  map(r, 25, w/2, 3000, 100);
+    let delayAMT = map(ellipseX, 0, w, 0, 90);
+    if(paramPlaySpeed) paramPlaySpeed.normalizedValue = playSpeedAMT;
+    if(paramDrive) paramDrive.normalizedValue = driveAMT;
+    if(paramLoop) paramLoop.normalizedValue = loopAMT;
+    if(paramDelay) paramDelay.normalizedValue = delayAMT;
 }
 
 function resumeAudio(){
     sketchStarted = true;
     startButton.style('opacity', '0')
 
+
     if(getAudioContext().state !== 'running'){
-        audioContext.resume()
+        audioContext.resume();
     }
+
 }
 
 function draw(){
-    background('rgba(255, 255, 255, 0.15)');
+    background('rgba(255, 255, 255, 0.5)');
+    if(sketchStarted == true){
+        let x = map(noise(xoff), 0, 1, 0, width);
+        xoff += 0.001;
+        ellipse(x, h/2, r*2, r*2);
+        ellipseX = x;
+
+        // Check for MouseOver
+        var d = dist(mouseX, mouseY, x, h/2);
+        if(d < r){
+            r += .1;
+        } else {
+            if(r > 25) {
+                r -= 1;
+            }
+        }
+        applyParameters();
+    }
 
 
 }
---
+++ experiments.md
@@ -0,0 +1,15 @@
+# Experiments and Responses
+
+A series of small experiments for reflection in games, primarily focusing on experimental interactions in p5.js and sound design in RNBO.
+
+### Experiment 1 / Jun 19, 2024
+
+It's a black circle. Touch it.
+
+https://mouseandthebillionaire.com/reflectiveStudies/01/
+
+When the user hovers over the black circle it begins to slowly grow in size while the audio detunes and becomes more complex.
+
+One interesting thing about this is that the act of holding the mouse over the circle becomes easier and easier and easier as the circle grows larger (until really the user doesn't need to do anything for it to continue). This feels in line with reflective or meditative experiences. It's often harder to get going, to decide to take time away from your thoughts or activities but then once you're going it's much easier to keep going. From my perspective the act of holding the mouse on the circle does bring about a bit of a calming experience (which, it's still to be determined wether or not \[probably not\] 'calming' is even a goal) But also, this feels focusing. There's just enough effort required of you from the start, and it is satisfying as those requirements become less and less.
+
+Sonically, the sounds become lower and slower as the mouseover interaction progresses, which feels right. Additionally, there is a distortion *added*, and while this may initially feel like a weird decision, I think it works in a [Low "Hey What"](https://lowtheband.bandcamp.com/album/hey-what) kind of way (which, I feel is very soothing/reflective, but maybe not to all)
